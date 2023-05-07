2a0abda3f15102224fab26fa2bb70c4624a31e5c
Vitalii Iakushev <vsiakushev@gmail.com>
2023-05-07 12:51:05 -0700

    Initial Commit

---
+++ PieCharts/PieChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartsApp.swift
+//  PieCharts
+//
+//  Created by Vitaliy Iakushev on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PieChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
